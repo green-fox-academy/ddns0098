f42879bcd0b87cb49e944d5d3d94b4db896d88b4
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-02-15 12:08:05 +0100

    gitignore

---
+++ week-01/day-03/src/tasks.java
@@ -0,0 +1,24 @@
+public class hellome {
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, Dénes!");
+
+        // Modify this program to print Humpty Dumpty riddle correctly
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("All the king's horses and all the king's men");
+        System.out.println("Couldn't put Humpty together again.");
+
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello, Peti!");
+        System.out.println("Hello, Laci!");
+        System.out.println("Hello, Zita!");
+    }
+}
+
