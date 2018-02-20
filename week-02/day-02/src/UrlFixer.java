574d0df97eb134bc387fda45dec204ca6baff236
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-02-20 12:08:48 +0100

    feat(UrkFixer)

---
+++ week-02/day-02/src/UrlFixer.java
@@ -0,0 +1,12 @@
+public class UrlFixer {
+    public static void main(String... args){
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+
+        // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+        // Also, the URL is missing a crutial component, find out what it is and insert it too!
+        // Try to solve it more than once using different String functions!
+
+        System.out.println(url.replace("bots", "odds").replace("https", "https:"));
+        System.out.println(url.substring(0,url.indexOf("s") + 1) + ":" + url.substring(url.indexOf("/"), url.indexOf("b")) + "odds");
+    }
+}
