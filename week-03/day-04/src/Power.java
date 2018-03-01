ae4f44bc272ec76abe0199acb2d367feedb6000a
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-03-01 10:41:46 +0100

    feat(Power)

---
+++ week-03/day-04/src/Power.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Power {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int power = scanner.nextInt();
+
+    System.out.println(powerN(n, power));
+  }
+
+  private static int powerN(int n, int power) {
+    if (power == 0) {
+      return 1;
+    } else {
+      return n * powerN(n, --power);
+    }
+  }
+}
