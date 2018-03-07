36d7d8d704721db3ef9008a797f2ff253f6e61a8
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-03-07 12:04:53 +0100

    feat(Cloneable): add Person class, Student class, override clone method

---
+++ week-04/day-03/src/cloneable/Student.java
@@ -0,0 +1,35 @@
+package cloneable;
+
+public class Student extends Person implements Cloneable{
+  private String previousOrganization;
+  private int skippedDays;
+
+  public  Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    skippedDays = 0;
+  }
+
+  public Student() {
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  @Override
+  public Student clone() {
+    Student newStudent = this;
+    return newStudent;
+  }
+
+  public void getGoal() {
+    System.out.println("My Goal: Be a junior software developer.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + ", a " + getAge() + " year old " + getGender() + " from " + previousOrganization + " who skipped " + skippedDays + " days.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+  }
+}
