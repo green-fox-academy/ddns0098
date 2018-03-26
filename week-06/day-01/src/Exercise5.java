a43a2479a46e253e8e1fd2332b0582c9294a02b5
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-03-26 12:53:57 +0200

    fix(Exercise5): remove for loop

--- week-06/day-01/src/Exercise5.java
+++ week-06/day-01/src/Exercise5.java
@@ -1,17 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Exercise5 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     String str = scanner.nextLine();
-    List<Character> chars = new ArrayList<>();
-    for (int i = 0; i < str.length(); i++) {
-      chars.add(str.charAt(i));
-    }
-    chars
-            .stream()
+    str.chars()
+            .mapToObj(c -> (char) c)
             .filter(Character::isUpperCase)
             .forEach(System.out::print);
   }
