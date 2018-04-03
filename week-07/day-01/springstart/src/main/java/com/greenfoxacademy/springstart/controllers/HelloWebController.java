e2d02c50159c217c435f858ccbaf60cc961fcf32
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-04-03 14:37:23 +0200

    feat(springstart): add counter to HelloWebController

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,13 +3,19 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  private AtomicLong count = new AtomicLong();
 
   @RequestMapping(value = "/web/greeting", produces = "text/plain;charset=UTF-8")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam("name") String name) {
+    model.addAttribute("name", name);
+    count.getAndIncrement();
+    model.addAttribute("count", count);
     return "greeting";
   }
 }
