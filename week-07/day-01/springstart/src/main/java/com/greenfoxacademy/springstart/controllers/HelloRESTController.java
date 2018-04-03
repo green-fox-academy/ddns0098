c1ea0b7d18111ecf4f1ce3633f8036eeb240f98d
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-04-03 12:38:32 +0200

    feat(springstart): update HelloRESTController class

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping("/greeting")
-  public Greeting greeting() {
-    return new Greeting(1,"Hello, World!");
+  public Greeting greeting(@RequestParam("name") String content) {
+    return new Greeting(1, content);
   }
 }
