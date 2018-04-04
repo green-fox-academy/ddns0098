b51792ca18299f26e066c265befccf67abd96477
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-04-04 11:38:56 +0200

    feat(bank): add formatter and currency

--- week-07/day-02/bank/src/main/java/com/greenfoxacademy/bank/controller/AccountController.java
+++ week-07/day-02/bank/src/main/java/com/greenfoxacademy/bank/controller/AccountController.java
@@ -5,14 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 @Controller
 public class AccountController {
+  private String currency = "Zebra";
 
   @GetMapping("/show")
   public String showAccount(Model model) {
+    NumberFormat formatter = new DecimalFormat("#0.00");
     BankAccount account = new  BankAccount("Simba", 2000, "lion");
     model.addAttribute("name", account.getName());
-    model.addAttribute("balance", account.getBalance());
+    model.addAttribute("balance", formatter.format(account.getBalance()));
+    model.addAttribute("currency", currency);
     model.addAttribute("animal", account.getAnimalType());
     return "show";
   }
