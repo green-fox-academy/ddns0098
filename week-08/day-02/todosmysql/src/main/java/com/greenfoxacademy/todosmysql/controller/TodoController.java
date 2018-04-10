c2a4624d7bcd8bd52fae66527f34913d873ac953
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-04-10 17:31:49 +0200

    feat(todosmysql): add edit functionality

--- week-08/day-02/todosmysql/src/main/java/com/greenfoxacademy/todosmysql/controller/TodoController.java
+++ week-08/day-02/todosmysql/src/main/java/com/greenfoxacademy/todosmysql/controller/TodoController.java
@@ -19,12 +19,12 @@ public class TodoController {
   }
 
   @RequestMapping(value = {"/","/list"})
-  public String list(Model model, @RequestParam(value = "isActive", required = false) boolean isActive) {
-    if (isActive) {
-      model.addAttribute("todos", todoRepository.findByDone(isActive));
-    } else {
-      model.addAttribute("todos", todoRepository.findAll());
+  public String list(Model model, @RequestParam(value = "isActive", required = false, defaultValue = "true") boolean isActive) {
+    if (!isActive) {
+      model.addAttribute("todos", todoRepository.findAllByDone(isActive));
+      return "todoslist";
     }
+      model.addAttribute("todos", todoRepository.findAll());
     return "todoslist";
   }
 
@@ -35,9 +35,7 @@ public class TodoController {
 
   @PostMapping("/add")
   public String add(@ModelAttribute(name = "title") String title) {
-    if (!title.equals("") && title != null) {
-      todoRepository.save(new Todo(title));
-    }
+    todoRepository.save(new Todo(title));
     return "redirect:/todo/";
   }
 
@@ -47,4 +45,15 @@ public class TodoController {
     return "redirect:/todo/";
   }
 
+  @GetMapping("/{id}/edit")
+  public String edit(Model model, @PathVariable long id) {
+    model.addAttribute("todo", todoRepository.findById(id).get());
+    return "edit";
+  }
+
+  @PostMapping("/{id}/edit")
+  public String edit(@ModelAttribute(name = "todo") Todo todo) {
+    todoRepository.save(todo);
+    return "redirect:/todo/";
+  }
 }
--- week-08/day-02/todosmysql/src/main/java/com/greenfoxacademy/todosmysql/repository/TodoRepository.java
+++ week-08/day-02/todosmysql/src/main/java/com/greenfoxacademy/todosmysql/repository/TodoRepository.java
@@ -3,7 +3,9 @@ package com.greenfoxacademy.todosmysql.repository;
 import com.greenfoxacademy.todosmysql.model.Todo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
-  public Todo findByDone(boolean done);
+  List<Todo> findAllByDone(boolean done);
 }
