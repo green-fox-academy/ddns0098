dd6fb69bc0c804f863965abc3b67adcdee43e46b
Dinók <35537168+ddns0098@users.noreply.github.com>
2018-04-10 12:06:06 +0200

    feat(todos):add template and connect with database

--- week-08/day-02/todos/src/main/java/com/greenfoxacademy/todos/controller/TodoController.java
+++ week-08/day-02/todos/src/main/java/com/greenfoxacademy/todos/controller/TodoController.java
@@ -1,11 +1,12 @@
 package com.greenfoxacademy.todos.controller;
 
+import com.greenfoxacademy.todos.model.Todo;
 import com.greenfoxacademy.todos.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/todo")
@@ -19,9 +20,9 @@ public class TodoController {
   }
 
   @RequestMapping(value = {"/","/list"})
-  @ResponseBody
   public String list(Model model) {
-    return "This is my first todo";
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todoslist";
   }
 
 }
--- week-08/day-02/todos/src/main/java/com/greenfoxacademy/todos/model/Todo.java
+++ week-08/day-02/todos/src/main/java/com/greenfoxacademy/todos/model/Todo.java
@@ -1,9 +1,39 @@
 package com.greenfoxacademy.todos.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   private String title;
   private boolean urgent;
   private boolean done;
 
+  public Todo(String title) {
+    this.title = title;
+  }
+
+  public Todo() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
 }
