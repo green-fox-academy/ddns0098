c312fb246e5e17559a31da9aebed4185a327d254
unknown <d.dns0098g@gmail.com>
2018-04-17 15:23:34 +0200

    fix(guardian): fix tests

--- week-09/day-02/src/main/java/com/greenfox/resttest/controllers/GuardianController.java
+++ week-09/day-02/src/main/java/com/greenfox/resttest/controllers/GuardianController.java
@@ -3,34 +3,32 @@ package com.greenfox.resttest.controllers;
 
 import com.greenfox.resttest.models.Groot;
 import com.greenfox.resttest.models.Yondu;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class GuardianController {
 
     @RequestMapping("/groot")
-    public Groot groot(@RequestParam(required = false) String message, HttpServletResponse response) {
+    public ResponseEntity groot(@RequestParam(required = false) String message) {
         if (message != null) {
-            return new Groot(message);
+            return ResponseEntity.status(HttpStatus.OK).body(new Groot(message));
+
         }
-        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-        return new Groot();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new Groot());
     }
 
     @RequestMapping("/yondu")
-    public Yondu yondu(@RequestParam(required = false) Double distance, @RequestParam(required = false) Double time,  HttpServletResponse response) {
+    public ResponseEntity yondu(@RequestParam(required = false) Double distance, @RequestParam(required = false) Double time) {
         if (distance == null) {
-            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-            return new Yondu("Please provide a distance!");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new Yondu("Please provide a distance!"));
         } else if (time == null) {
-            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-            return new Yondu("Please provide a time!");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new Yondu("Please provide a time!"));
         } else {
-            return new Yondu(distance,time);
+            return ResponseEntity.status(HttpStatus.OK).body(new Yondu(distance,time));
         }
     }
 }
