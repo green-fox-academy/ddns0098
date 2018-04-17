cc761021a0a715d93540affa90d8ceeb809a14a2
unknown <d.dns0098g@gmail.com>
2018-04-17 12:00:47 +0200

    feat(doubling): add test cases

---
+++ week-09/rest/src/test/java/com/greenfoxacademy/rest/controllers/DoublingControllerTest.java
@@ -0,0 +1,48 @@
+package com.greenfoxacademy.rest.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class DoublingControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void doubling() throws Exception {
+        mvc.perform(get("/doubling?input=15"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'received' : 15, 'result' : 30}"));
+    }
+
+    @Test
+    public void doublingNoInput() throws Exception {
+        mvc.perform(get("/doubling"))
+                .andExpect(content().json("{'error' : 'Please provide an input!'}"));
+    }
+}
